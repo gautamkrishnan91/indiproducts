b0d753c60f592b2952209cbf3a94cacc52f988c7
Gautam Krishnan <gkthegr8@gmail.com>
2013-09-18 00:18:28 +0530

    Added About page

--- about.php
+++ about.php
@@ -8,6 +8,17 @@
 		include 'header.php';
         echo"
  		<div id='content-wrapper'>
+ 			<div class='about-wrapper'>
+ 				
+	 			<div class='about-main-photocontainer'>
+	 				<div class='about-main-photo'></div>
+	 				<div class='about-main-name'>Gautam Krishnan</div>
+	 			</div>
+	 			<div class='about-separator'>Founder</div>
+	 			<div class='about-main-content'>
+	 				Founder and CEO. Wafer ice cream apple pie fruitcake jelly-o sesame snaps cookie. Halvah cookie applicake tootsie roll topping gummies cheesecake liquorice. Danish chocolate gummies topping gummies marzipan fruitcake unerdwear.com tiramisu. Biscuit sweet roll cheesecake oat cake apple pie chocolate bar candy gingerbread icing. Candy chocolate liquorice. Tiramisu dragée candy canes sweet roll dragée.
+	 			</div>
+ 			</div>
  		</div>
         ";
 ?>

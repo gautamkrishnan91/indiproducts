ad1b548fa718bf1b75a82ed1f5b15405bb1d02b8
Gautam Krishnan <gkthegr8@gmail.com>
2013-09-18 23:52:26 +0530

    Added Subcategories

--- js/dropdown.js
+++ js/dropdown.js
@@ -5,6 +5,7 @@ function submitSubCategory(sender){
     if(sid==1){
     	document.indian_form.prod_subcat.options.length=0;
 
+        document.indian_form.prod_subcat.options[0]=new Option("Baby Foods", "sportsvalue", true, false);
     	document.indian_form.prod_subcat.options[0]=new Option("Biscuits", "sportsvalue", true, false);
     	document.indian_form.prod_subcat.options[1]=new Option("Chips", "musicvalue", false, false);
 		document.indian_form.prod_subcat.options[2]=new Option("Chocolates", "musicvalue", false, false);
@@ -27,10 +28,33 @@ function submitSubCategory(sender){
     if(sid==3){
     	document.indian_form.prod_subcat.options.length=0;
 
-    	document.indian_form.prod_subcat.options[0]=new Option("Coffee/Tea", "sportsvalue", true, false);
-    	document.indian_form.prod_subcat.options[1]=new Option("Juice", "musicvalue", false, false);
-    	document.indian_form.prod_subcat.options[2]=new Option("Mineral Water", "musicvalue", false, false);
-    	document.indian_form.prod_subcat.options[3]=new Option("Powdered drink mixes", "musicvalue", false, false);
-    	document.indian_form.prod_subcat.options[4]=new Option("Soft Drinks", "musicvalue", false, false);
+    	document.indian_form.prod_subcat.options[0]=new Option("Clothing", "sportsvalue", true, false);
+    	document.indian_form.prod_subcat.options[1]=new Option("Footwear", "musicvalue", false, false);
+    	document.indian_form.prod_subcat.options[2]=new Option("Watches", "musicvalue", false, false);
+    	document.indian_form.prod_subcat.options[3]=new Option("Wallets", "musicvalue", false, false);
+    	document.indian_form.prod_subcat.options[4]=new Option("Bags", "musicvalue", false, false);
+        document.indian_form.prod_subcat.options[5]=new Option("Other Fashion and Clothing", "musicvalue", false, false);
+    }
+    if(sid==4){
+        document.indian_form.prod_subcat.options.length=0;
+
+        document.indian_form.prod_subcat.options[0]=new Option("Mobiles and Tablets", "sportsvalue", true, false);
+        document.indian_form.prod_subcat.options[1]=new Option("Computers and Laptops", "musicvalue", false, false);
+        document.indian_form.prod_subcat.options[2]=new Option("Mobile and Computer Accessories", "musicvalue", false, false);
+        document.indian_form.prod_subcat.options[3]=new Option("TVs", "musicvalue", false, false);
+        document.indian_form.prod_subcat.options[4]=new Option("Large Household Appliances", "musicvalue", false, false);
+        document.indian_form.prod_subcat.options[5]=new Option("Small Household Appliances", "musicvalue", false, false);
+        document.indian_form.prod_subcat.options[6]=new Option("Other Electronics", "musicvalue", false, false);
+    }
+    if(sid==5){
+        document.indian_form.prod_subcat.options.length=0;
+
+        document.indian_form.prod_subcat.options[0]=new Option("Household Provisions", "sportsvalue", true, false);
+        document.indian_form.prod_subcat.options[1]=new Option("Cleaning Products", "musicvalue", false, false);
+        document.indian_form.prod_subcat.options[2]=new Option("Stationaries", "musicvalue", false, false);
+        document.indian_form.prod_subcat.options[3]=new Option("Household Electricals", "musicvalue", false, false);
+        document.indian_form.prod_subcat.options[4]=new Option("Large Household Appliances", "musicvalue", false, false);
+        document.indian_form.prod_subcat.options[5]=new Option("Small Household Appliances", "musicvalue", false, false);
+        document.indian_form.prod_subcat.options[6]=new Option("Other Electronics", "musicvalue", false, false);
     }
 }

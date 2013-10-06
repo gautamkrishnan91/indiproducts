792e247b72476a96f358e463a3e14723deef8d15
gkthegr8 <gkthegr8@gmail.com>
2013-10-06 18:27:32 +0530

    Search 1.0 implemented

--- search.php
+++ search.php
@@ -28,31 +28,45 @@
  				else{
  					$category = $_GET['category'];
  					$search = $_GET['search'];
- 					$query = "SELECT * FROM indian_temp WHERE name = $search AND category = $category";
-					$result = mysqli_query($con, $query);
-			 		echo"<table class='searchresults'>
-			 		<tr>
-			 			<th class='name'>Product Name</th>
-			 			<th class='brand'>Brand</th>
-			 			<th class='contact'>Contact</th>
-			 			<th class='link'>Website</th>
-			 			<th class='category'>Category</th>
-			 			<th class='subcategory'>Sub-category</th>
-			 			<th class='comments'>Comments</th>
-			 		</tr>
-			 		<tr><td>";
-			 			
-			 		while($row = mysqli_fetch_array($result)){
-						echo $row['name']."</td><td>";
-						echo $row['company']."</td><td>";
-						echo $row['contact']."</td><td class='link'>";
-						echo $row['website']."</td><td>";
-						echo $row['category']."</td><td>";
-						echo $row['subcategory']."</td><td>";
-						echo $row['comments']."</td>";
-					}
 
- 					echo "</table></div>";
+ 					$query = "SELECT * FROM indian_temp WHERE name LIKE '%".mysql_real_escape_string($search)."%'";
+ 					
+					$result = mysqli_query($con, $query);
+					
+					if($result === FALSE){
+						echo"<div class='noSearchQuery'>Sorry, something went wrong on our side!</div>";
+					}
+					else{
+						$num_rows = $result->num_rows;
+						if ($num_rows==0){
+							echo"<div class='noSearchQuery'>No results found for your search query!</div>";		
+						}
+						else{
+					 		echo"<table class='searchresults'>
+					 		<tr>
+					 			<th class='name'>Product Name</th>
+					 			<th class='brand'>Brand</th>
+					 			<th class='contact'>Contact</th>
+					 			<th class='link'>Website</th>
+					 			<th class='category'>Category</th>
+					 			<th class='subcategory'>Sub-category</th>
+					 			<th class='comments'>Comments</th>
+					 		</tr>
+					 		<tr><td>";
+					 			
+					 		while($row = mysqli_fetch_array($result)){
+								echo $row['name']."</td><td>";
+								echo $row['company']."</td><td>";
+								echo $row['contact']."</td><td class='link'>";
+								echo $row['website']."</td><td>";
+								echo $row['category']."</td><td>";
+								echo $row['subcategory']."</td><td>";
+								echo $row['comments']."</td>";
+							}
+
+		 					echo "</table></div>";
+		 				}
+ 					}
         		}
         	}
         	else{

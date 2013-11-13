608cae27b9fd2300209c31f0735775a2d1735162
gkthegr8 <gkthegr8@gmail.com>
2013-11-13 23:47:03 +0530

    Modified index

--- index.php
+++ index.php
@@ -50,8 +50,8 @@
                 </ul>
             </div>
             <div class='clear'></div>
-                Gingerbread soufflé tootsie roll fruitcake jelly beans tootsie roll chocolate bar donut dessert. Candy canes cupcake unerdwear.com sugar plum jelly-o. Tart cupcake cotton candy applicake chocolate jelly-o ice cream topping. Topping donut candy powder cotton candy. Liquorice sweet roll jelly. Sweet roll apple pie dragée gingerbread jelly beans gingerbread sweet roll sugar plum muffin. Sweet macaroon jelly-o gummi bears chocolate chupa chups caramels.
-                Chupa chups chocolate bar muffin macaroon powder gingerbread caramels. Chupa chups gingerbread applicake bonbon carrot cake pudding oat cake. Ice cream unerdwear.com dessert lollipop bonbon brownie sugar plum tootsie roll chocolate cake. Gummies pie icing dessert lemon drops wafer gummies. Liquorice macaroon wafer soufflé candy canes. Cupcake jelly-o liquorice dessert cupcake carrot cake lollipop pastry.
+                <div class='intro-para'>Gingerbread soufflé tootsie roll fruitcake jelly beans tootsie roll chocolate bar donut dessert. Candy canes cupcake unerdwear.com sugar plum jelly-o. Tart cupcake cotton candy applicake chocolate jelly-o ice cream topping. Topping donut candy powder cotton candy. Liquorice sweet roll jelly. Sweet roll apple pie dragée gingerbread jelly beans gingerbread sweet roll sugar plum muffin. Sweet macaroon jelly-o gummi bears chocolate chupa chups caramels.</div>
+                <div class='intro-para'>Chupa chups chocolate bar muffin macaroon powder gingerbread caramels. Chupa chups gingerbread applicake bonbon carrot cake pudding oat cake. Ice cream unerdwear.com dessert lollipop bonbon brownie sugar plum tootsie roll chocolate cake. Gummies pie icing dessert lemon drops wafer gummies. Liquorice macaroon wafer soufflé candy canes. Cupcake jelly-o liquorice dessert cupcake carrot cake lollipop pastry.</div>
             </div>
         </div>
 	</body>

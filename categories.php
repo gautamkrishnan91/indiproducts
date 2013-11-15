5c1c0717477adf52266c912f73eaec5bd8eae735
gkthegr8 <gkthegr8@gmail.com>
2013-11-16 00:24:29 +0530

    Updated categories - added individual categories check

--- categories.php
+++ categories.php
@@ -1,8 +1,8 @@
 <?php
-
+    $_GET['category'];
 	require_once 'connection.php';	
+    $category = check_input($_GET['category']);
 	$con = mysqli_connect("sql102.byethost11.com","b11_13826268","6fb371qx","b11_13826268_products_store");
-
 echo"
 <html>
 	<head>";
@@ -12,11 +12,8 @@ echo"
 	</head>
 	<body>";
 		include 'header.php';
-		$query = "SELECT DISTINCT company FROM indian_temp";
-		$result = mysqli_query($con, $query);
-		if($result === FALSE){
-			echo"<div class='noSearchQuery'>Sorry, something went wrong on our side!</div>";
-		}
+        $category = strtolower($category);
+        if(($category == '') || ($category == 'food') || ($category == 'beverages') || ($category == 'fashion') || ($category == 'electronics') || ($category == 'consumables') || ($category == 'twowheelers') || ($category == 'homeandliving') || ($category == 'other')){
         echo"
  		<div id='content-wrapper'>
             <div class='categories-left'>
@@ -34,6 +31,13 @@ echo"
             </div>
             <div class='categories-right'>
                 <ul class='categories-master'>";
+                    if($category == ''){
+                    $query = "SELECT DISTINCT company FROM indian_temp";
+                    }
+                    else{
+                    $query = "SELECT DISTINCT company FROM indian_temp WHERE category LIKE '".$category."'";
+                    }
+                    $result = mysqli_query($con, $query);
                     $resultSet = array();
                     while($row = mysqli_fetch_array($result)){
                         $resultSet[] = $row['company'];
@@ -49,24 +53,12 @@ echo"
                                         if(strcasecmp($charac, $resultSet[$i][0])==0)
                                         echo "<li>".$resultSet[$i]."</li>";
                                     }
-                                echo"</li>
-                                </ul>";
+                                echo"</ul>
+                                </li>";
                             }
                         }
                     }
-            // for($asci = 97; $asci < 123; $asci++)
-            // {
-            //     $charac = chr($asci);
-            //     echo $charac;
-                
-            //         echo $charac;
-            //         if(strcasecmp($charac, $row['company'][0])==0)
-            //         echo $row['company'];
-                
-            // }
             
-            echo"
- 		</div>
-";
-
+            echo"</ul></div>";}
+        echo"</div></body>"
 ?>

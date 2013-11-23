693cda8a7a539b32bc9ab256682c3c81be41092e
gkthegr8 <gkthegr8@gmail.com>
2013-11-23 15:29:57 +0530

    Products page done

--- product.php
+++ product.php
@@ -10,14 +10,61 @@ echo"
         echo"
 	</head>
 	<body>";
-		include 'header.php';
+        include 'header.php';
+        echo"
+        <div id='content-wrapper'>";
         $product = strtolower($product);
         $query = "SELECT * FROM indian_temp WHERE name LIKE '".$product."' LIMIT 1";
         $result = mysqli_query($con, $query);
         if(mysqli_num_rows($result) != 0){
+        while($row = mysqli_fetch_array($result)){
         echo"
- 		<div id='content-wrapper'>    
-            <div>Hi</div>
-        </div>";}
-        echo"</div></body>"
+            <div class='brand-singleContainer'>
+                <h3>Product Information:</h3>
+                <table>
+                    <tr>
+                        <td class='left'>Product Name:</td>
+                        <td>".$row['name']."</td>
+                    </tr>
+                    <tr>
+                        <td class='left'>Brand:</td>
+                        <td>".$row['company']."</td>
+                    </tr>";
+                    if($row['contact']!=""){
+                    echo"
+                    <tr>
+                        <td class='left'>Contact:</td>
+                        <td>".$row['contact']."</td>
+                    </tr>";
+                    }
+                    if($row['website']!=""){
+                    echo"
+                    <tr>
+                        <td class='left'>Website:</td>
+                        <td><a href='".$row['website']."'>".$row['website']."</a></td>
+                    </tr>";
+                    }
+                    echo"
+                    <tr>
+                        <td class='left'>Category:</td>
+                        <td>".$row['category']."</td>
+                    </tr>
+                    <tr>
+                        <td class='left'>Sub Category:</td>
+                        <td>".$row['subcategory']."</td>
+                    </tr>";
+                    if($row['comments']!=""){
+                    echo"
+                    <tr>
+                        <td class='left'>Comments:</td>
+                        <td>".$row['comments']."</td>
+                    </tr>";
+                    }
+                    echo"
+                </table>
+            </div>
+        ";
+        }
+        }
+        echo"</div></div></body>"
 ?>
